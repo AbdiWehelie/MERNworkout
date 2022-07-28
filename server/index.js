f9c9e6e8947e952355b16f5dc01653512490fad2
AbdiWehelie <devwehelie@gmail.com>
2022-07-27 21:16:23 -0400

    -Retrying .env connection

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 
 // TODO: make .env of the MongoDB connection link
 mongoose.connect(
-  "mongodb+srv://user123:Password123@cluster0.iozvx4n.mongodb.net/workoutdb?retryWrites=true&w=majority"
+  process.env.DATA
 )
 
 let db = mongoose.connection;
