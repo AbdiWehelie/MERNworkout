246607abb4263ba394d252c6ac64edf6973da41b
AbdiWehelie <devwehelie@gmail.com>
2022-07-27 20:57:21 -0400

    -.env files

--- server/index.js
+++ server/index.js
@@ -14,9 +14,9 @@ app.use(cors());
 
 require('dotenv').config();
 
-
+// TODO: make .env of the MongoDB connection link
 mongoose.connect(
-  "mongodb+srv://user123:Password123@cluster0.iozvx4n.mongodb.net/workoutdb?retryWrites=true&w=majority"
+  process.env.DATA
 )
 
 let db = mongoose.connection;
